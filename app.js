342d5571e32e902d3dc70a0caa67c0cc54d7f232
Fotis Paraskevopoulos <fotis@existanze.com>
2013-12-02 11:24:54 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+    , passport = require('passport')
+    , util = require('util')
+    ,FacebookStrategy = require('passport-facebook').Strategy
+    , path = require('path');
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Instagram profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+var RedisStore = require('connect-redis')(express);
+
+var redisOpts ={};
+
+if(process.env['REDIS_URL']){
+
+    console.log("We are on dokku");
+    var url = require('url').parse(process.env['REDIS_URL'])
+    redisOpts ={
+        port: url.port
+        ,host: url.hostname
+    }
+}else{
+    redisOpts = {
+        port:'6379'
+        ,host: 'localhost'
+    }
+}
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3002);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
